8d941011b91707b9425383deb9fb96633abd2cb2
Henning Eiben <eiben@busitec.de>
2018-10-26 11:30:05 +0200

    adding progress-bars

--- NewSharePointMigrationProject/create-structure.ps1
+++ NewSharePointMigrationProject/create-structure.ps1
@@ -3,6 +3,9 @@
     Erstellt eine Infrastruktur auf der Basis von Migrationen
 .DESCRIPTION
     TBD
+.NOTES
+    Author: Henning Eiben
+    Requires: SharePointOnline PnP-PowerShell Module >= 3.0.0.0
 .PARAMETER webUrl
     Die URL an die die Migration angewendet werden soll
 .PARAMETER targetDeployment
@@ -11,13 +14,17 @@
     Optional: Benutzername zur Authentifizierung an der WebURL
 .PARAMETER password
     Optional: Passwort zur Authentifizierung an der WebURL als SecureString
+.LINK
+    https://github.com/SharePoint/PnP-PowerShell
 .EXAMPLE
     Create-Structure -webUrl https://acme.local.com/sites/foo
+.EXAMPLE
+    Create-Structure -webUrl https://acme.local.com/sites/foo -targetDeployment 004
 .EXAMPLE
     Create-Structure -webUrl https://acme.local.com/sites/foo -username bigboss -password ('password' | ConvertTo-SecureString -AsPlainText -Force)
 #>
 param (
-[parameter(Mandatory = $true)]
+    [parameter(Mandatory = $true)]
     [string]$webUrl,
     [parameter(Mandatory = $false)]
     [string]$targetDeployment,
@@ -46,29 +53,29 @@ try {
     if ($PLASTER_PARAM_Edition -eq 'Online')
     {
 @'
-Import-Module "SharePointPnPPowerShellOnline" 3>$null
+Import-Module "SharePointPnPPowerShellOnline" -MinimumVersion 3.0.0.0 3>$null
 ### Authentication
 if ($username -and $password) {
     $cred = New-Object System.Management.Automation.PSCredential -argumentlist $username, $password
-    Connect-PnPOnline -Url $webUrl -Credentials $cred        
+    Connect-PnPOnline -Url $webUrl -Credentials $cred -SkipTenantAdminCheck
 }
 else {
-    Connect-PnPOnline -Url $webUrl -UseWebLogin
+    Connect-PnPOnline -Url $webUrl -UseWebLogin -SkipTenantAdminCheck
 }
 '@
     }
     else 
     {
 @'
-Import-Module "SharePointPnPPowerShell2013" 3>$null
+Import-Module "SharePointPnPPowerShell2013" -MinimumVersion 3.0.0.0 3>$null
 ### Authentication
 $ctx = New-Object Microsoft.SharePoint.Client.ClientContext($webUrl)
 if ($username -and $password) {
     $cred = New-Object System.Management.Automation.PSCredential -argumentlist $username, $password
-    Connect-PnPOnline -Url $webUrl -Credentials $cred        
+    Connect-PnPOnline -Url $webUrl -Credentials $cred -SkipTenantAdminCheck
 }
 else {
-    Connect-PnPOnline -Url $webUrl -CurrentCredentials
+    Connect-PnPOnline -Url $webUrl -CurrentCredentials -SkipTenantAdminCheck
 }
 '@
     }
@@ -80,10 +87,14 @@ catch {
 
 
 $allScripts = Get-ChildItem -Path "$dp0\migrations\" -Directory
+$allScriptsCount = $allScripts.Length
+$i = 0
 
 foreach ($currentScript in $allScripts) {    
     $currentDeployment = $currentScript.Name
-    $previousDeployment = "{0:000}" -f [math]::Max($currentDeployment - 1, 0);
+    $previousDeployment = "{0:000}" -f [math]::Max($currentDeployment - 1, 0)
+
+    Write-Progress -Id 0 -Activity "Migration $currentDeployment" -Status "Processing" -PercentComplete ($i / $allScriptsCount * 100)
 
     Invoke-Migration `
         -fieldName "<%= $PLASTER_PARAM_FieldName %>" `
@@ -92,4 +103,6 @@ foreach ($currentScript in $allScripts) {
         -previousDeployment $previousDeployment `
         -down:$false `
         -path "$dp0\migrations\$currentScript"
+
+    $i++
 }
